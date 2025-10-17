cfcd0b32e80c03f509c61abfaed87448c6f4c5f1
Reera <126323642+FatemehTehranizadeh@users.noreply.github.com>
2025-10-17 15:33:29 +0330

    Writing methods

--- internal/limiter/leaky_bucket.go
+++ internal/limiter/leaky_bucket.go
@@ -2,12 +2,17 @@ package internal
 
 import (
 	"context"
+	"fmt"
+	"math"
 	"sync"
+	"time"
 )
 
 type LeakyBucketLimiter struct {
 	rate     float64
 	capacity int64
+	lastLeak time.Time
+	tokens int64
 	store    Store
 	mu       sync.Mutex
 	cond     *sync.Cond // used to block Waiters when no tokens present (or per-key conds)
@@ -15,36 +20,85 @@ type LeakyBucketLimiter struct {
 	pool     *sync.Pool // reuse Reservation objects or small buffers
 }
 
-func (tbl *TokenBucketLimiter) Allow(ctx context.Context, key string) bool {
-	return true
+func (lbl *LeakyBucketLimiter) Allow(ctx context.Context, key string) bool {
+	select {
+	case <-ctx.Done():
+		return false
+	default:
+		lbl.mu.Lock()
+		defer lbl.mu.Unlock()
+		if lbl.capacity > 0 {
+			lbl.capacity--
+			return true
+		}
+	}
+	return false
 }
 
-func (tbl *TokenBucketLimiter) TryAcquire(key string, n int) bool { //pick n tokens up
-	return true
+func (lbl *LeakyBucketLimiter) TryAcquire(key string, n int64) bool { //pick n tokens up
+	if n <= lbl.capacity {
+		lbl.mu.Lock()
+		defer lbl.mu.Unlock()
+		lbl.capacity -= n
+		return true
+	}
+	return false
 }
 
-func (tbl *TokenBucketLimiter) Wait(ctx context.Context, key string) error { //block until token available using Cond or channel
-	return ctx.Err()
+func (lbl *TokenBucketLimiter) Reserve(ctx context.Context, key string) (Reservation, bool) {
+	select {
+	case <-ctx.Done():
+		fmt.Println("Request timeout.")
+		return nil, false
+	default:
+		lbl.mu.Lock()
+		defer lbl.mu.Unlock()
+		if lbl.capacity == 0 {
+			lbl.cond.Wait()
+		}
+		lbl.capacity --
+		fmt.Println("The request allows!")
+	}
+	return nil
+
 }
 
-func (tbl *TokenBucketLimiter) Refill() { //background goroutine to periodically refill tokens
-	
+func (lbl *LeakyBucketLimiter) Wait(ctx context.Context, key string) error { //block until token available using Cond or channel
+	select {
+	case <-ctx.Done():
+		fmt.Println("Request timeout.")
+		return ctx.Err()
+	default:
+		lbl.mu.Lock()
+		defer lbl.mu.Unlock()
+		if lbl.capacity == 0 {
+			lbl.cond.Wait()
+		}
+		lbl.capacity --
+		fmt.Println("The request allows!")
+		return nil	
+	}
+	return nil
 }
 
-func (tbl *TokenBucketLimiter) Stats(key string) Stats { //uses store to read counters
+func (lbl *LeakyBucketLimiter) Refill() { //background goroutine to periodically refill tokens
+	lbl.mu.Lock()
+	defer lbl.mu.Unlock()
+	leakRate := time.Duration(math.Round(lbl.rate))
+	now := time.Now()
+	elapsedTime := now.Sub(lbl.lastLeak)
+	if elapsedTime >= leakRate {
+		lbl.capacity += int64(lbl.rate)
+		lbl.cond.Signal()
+	}
+	lbl.lastLeak = lbl.lastLeak.Add(leakRate)	
+
+}
+
+func (lbl *LeakyBucketLimiter) Stats(key string) Stats { //uses store to read counters
 	return Stats{}
 }
 
-func (tbl *TokenBucketLimiter) Close() { //stop refill goroutine, broadcast cond.
-	
+func (lbl *LeakyBucketLimiter) Close() { //stop refill goroutine, broadcast cond.
+
 }
-
-
-
-
-
-
-
-
-
-
